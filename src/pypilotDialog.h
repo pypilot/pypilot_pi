7674771a884f7fcf8d39c694d2586338215861e2
Sean D'Epagnier <seandepagnier@gmail.com>
2018-06-09 01:13:53 -0400

    correct manual control direction

--- src/pypilotDialog.h
+++ src/pypilotDialog.h
@@ -45,10 +45,10 @@ public:
 private:
     void OnAP( wxCommandEvent& event );
     void OnMode( wxCommandEvent& event );
-    void OnManualPortLong( wxCommandEvent& event ) { Manual(-1.8); }
-    void OnManualPortShort( wxCommandEvent& event ) { Manual(-.6); }
-    void OnManualStarboardShort( wxCommandEvent& event ) { Manual(.6); }
-    void OnManualStarboardLong( wxCommandEvent& event ) { Manual(1.8); }
+    void OnManualPortLong( wxCommandEvent& event ) { Manual(1.8); }
+    void OnManualPortShort( wxCommandEvent& event ) { Manual(.6); }
+    void OnManualStarboardShort( wxCommandEvent& event ) { Manual(-.6); }
+    void OnManualStarboardLong( wxCommandEvent& event ) { Manual(-1.8); }
     void OnGains( wxCommandEvent& event );
     void OnConfiguration( wxCommandEvent& event );
     void OnCalibration( wxCommandEvent& event );
