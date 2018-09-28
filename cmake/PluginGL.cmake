f1125adab275227d93c6fac138cf8641bb4a4126
Sean D'Epagnier <seandepagnier@gmail.com>
2018-09-27 20:51:40 -0400

    add missing file

---
+++ cmake/PluginGL.cmake
@@ -0,0 +1,12 @@
+##---------------------------------------------------------------------------
+## Author:      Sean D'Epagnier
+## Copyright:   
+## License:     GPLv3
+##---------------------------------------------------------------------------
+
+SET(SRC_PLUGINGL
+          src/plugingl/pidc.cpp
+          src/plugingl/pi_shaders.cpp
+          src/plugingl/TexFont.cpp
+          src/plugingl/qtstylesheet.cpp
+          )
