aba92a4709d5926c8dc46cdb5778f1c2c2616c44
dsr <bdbcat@yahoo.com>
2021-06-25 11:33:12 -0400

    Correct Android ARM64->aarch64

--- cmake/PluginSetup.cmake
+++ cmake/PluginSetup.cmake
@@ -202,7 +202,7 @@ if(NOT WIN32 AND NOT QT_ANDROID)
 else(NOT WIN32 AND NOT QT_ANDROID)
     set(ARCH "x86_64")
     if(_wx_selected_config MATCHES "androideabi-qt-arm64")
-        set(ARCH "arm64")
+        set(ARCH "aarch64")
         # android cannot used graphics context is wxWidgets as it does not exist
         set(wxUSE_GRAPHICS_CONTEXT 0)
     endif(_wx_selected_config MATCHES "androideabi-qt-arm64")
